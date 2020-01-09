a1a2c6e8879aa8e3eea5ea7aaa7edfac4044335d
chae1008 <59547311+chae1008@users.noreply.github.com>
2020-01-09 15:22:24 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, Sungshin!"
+    return "Hello, babo!"
 
 @app.route("/hello")
 def hello_world2():
     return "Hello, Sungshin2!"
+
+@app.route("/hello2")
+def hello_world3():
+    return "Hello, Hyejin!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
